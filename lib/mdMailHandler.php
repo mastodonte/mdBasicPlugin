771873fd41f317270e235f8ec578ea1a4f7f8b35
maui <niko@mauitz.com>
2013-01-30 19:56:16 -0200

    hotfix on sendMailPhp method using php mail funcion

--- lib/mdMailHandler.php
+++ lib/mdMailHandler.php
@@ -183,13 +183,17 @@ class mdMailHandler {
     if (!is_array($recipients))
       $recipients = explode(',', $recipients);
 
+    $mdMailXMLHandler = new mdMailXMLHandler();
+    $sendermail = $mdMailXMLHandler->getEmail();
+
     foreach ($recipients as $recipient) {
 
-      $mdMailXMLHandler = new mdMailXMLHandler();
-      $headers = 'MIME-Version: 1.0' . "\r\n";
-      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-      $headers .= 'From: ' . $from['name'] . '<' . $from['email'] . '>';
-      $headers .= "\r\n";
+      
+      $headers = 'MIME-Version: 1.0' . "\n";
+      $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\n";
+      $headers = "From:" . $from['name'] . " <" . $sendermail . ">\n";
+      $headers .= "Reply-To: ".$from['email'] . "\n";
+      
       // Mail it
       if (!mail($recipient, $subject, $body, $headers))
         return false;
