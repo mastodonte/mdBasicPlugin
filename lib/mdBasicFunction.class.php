8192a19f2c3b9162b98e22d993018959cbede57a
maui <niko@mauitz.com>
2013-06-07 22:29:28 -0300

    add cammel case method

--- lib/mdBasicFunction.class.php
+++ lib/mdBasicFunction.class.php
@@ -671,4 +671,24 @@ class mdBasicFunction {
     return $text;
   }
 
+
+
+  public static function strtocamel($str, $capitalizeFirst = false, $allowed = 'A-Za-z0-9') {
+      return ucwords(strtolower($str));
+      return preg_replace(
+          array(
+              '/([A-Z][a-z])/e', // all occurances of caps followed by lowers
+              '/([a-zA-Z])([a-zA-Z]*)/e', // all occurances of words w/ first char captured separately
+              '/[^'.$allowed.']+/e', // all non allowed chars (non alpha numerics, by default)
+              '/^([a-zA-Z])/e' // first alpha char
+          ),
+          array(
+              '" ".$1', // add spaces
+              'strtoupper("$1").strtolower("$2")', // capitalize first, lower the rest
+              '', // delete undesired chars
+              'strto'.($capitalizeFirst ? 'upper' : 'lower').'("$1")' // force first char to upper or lower
+          ),
+          $str
+      );
+  }
 }
